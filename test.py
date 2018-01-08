7975278fa6b11ec8c1da78a49f7f3f058a38632e
William Lorey <wwlorey@gmail.com>
2018-01-08 16:52:22 -0600

    Send email alert upon regex or connection failure

--- test.py
+++ test.py
@@ -1,11 +1,36 @@
 import os
 import re
+import smtplib
+import credentials as cred
 from selenium import webdriver
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+ambigSubject = 'ERROR | Twitter Bot v0r0'
+ambigBody = 'Error in quote extraction or source connection :(' 
+toAddr = 'wwlorey@gmail.com'
+
+def sendEmail(subject, body, toAddr):
+  msg = MIMEMultipart('alternative')
+
+  # Message parameters
+  msg['Subject'] = subject
+  msg['From'] = cred.fromAddr
+  msg['To'] = toAddr
+
+  # Message body
+  msg.attach(MIMEText(body, 'plain'))
+
+  s = smtplib.SMTP_SSL('smtp.gmail.com')
+  s.login(cred.fromAddr, cred.password)
+
+  s.sendmail(cred.fromAddr, [toAddr], msg.as_string())
+  s.quit()
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome('C:\Program Files\ChromeDriver\chromedriver.exe', chrome_options=options)
-driver.get('http://wwlorey.x10host.com/quote_of_the_day.html')
+driver.get('http://wwlorey.x10host.com/quote_of_the_day.htmll')
 
 match = re.search("Today's\sQuote(</b><br />|</b><br/>|<br/>)[\n\r\s]([\w\s]+\S).*\n.*_blank\">(.+)</a>", driver.page_source)
 
@@ -13,7 +38,7 @@ try:
   if match is None:
     raise Exception
 except Exception:
-  print(':-(')
+  sendEmail(ambigSubject, ambigBody, toAddr)
 else:
   print(match.group(2), match.group(3))
 
