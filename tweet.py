3d1b254a437bf9bbac547d2300013e95c99526e2
William Lorey <wwlorey@gmail.com>
2018-01-09 14:15:09 -0600

    Add looping & tweeting

--- tweet.py
+++ tweet.py
@@ -1,12 +1,21 @@
-import os, re, smtplib, tweepy, time, sis
+import os, re, smtplib, tweepy, time, sys
 import credentials as cred
 from selenium import webdriver
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# Variables
 ambigSubject = 'ERROR | Twitter Bot v0r0'
 ambigBody = 'Error in quote extraction or source connection :(' 
 toAddr = 'wwlorey@gmail.com'
+SECONDS_IN_DAY = 24 * 60 * 60
+
+
+# return: twitter API object authenticated with quote_bot_ credentials
+def getTwitterAPI():
+  auth = tweepy.OAuthHandler(cred.CONSUMER_KEY, cred.CONSUMER_SECRET)
+  auth.set_access_token(cred.ACCESS_KEY, cred.ACCESS_SECRET)
+  return tweepy.API(auth)
 
 def sendEmail(subject, body, toAddr):
   msg = MIMEMultipart('alternative')
@@ -28,16 +37,23 @@ def sendEmail(subject, body, toAddr):
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome('C:\Program Files\ChromeDriver\chromedriver.exe', chrome_options=options)
-driver.get('http://wwlorey.x10host.com/quote_of_the_day.htmll')
 
-match = re.search("Today's\sQuote(</b><br />|</b><br/>|<br/>)[\n\r\s]([\w\s]+\S).*\n.*_blank\">(.+)</a>", driver.page_source)
 
-try:
-  if match is None:
-    raise Exception
-except Exception:
-  sendEmail(ambigSubject, ambigBody, toAddr)
-else:
-  print(match.group(2), match.group(3))
+while True:
+  driver.get('http://wwlorey.x10host.com/quote_of_the_day.html')
 
-driver.close()
+  match = re.search("Today's\sQuote(</b><br />|</b><br/>|<br/>)[\n\r\s]([\w\s]+\S).*\n.*_blank\">(.+)</a>", driver.page_source)
+
+  try:
+    if match is None:
+      raise Exception
+  except Exception:
+    sendEmail(ambigSubject, ambigBody, toAddr)
+  else:
+    quote = match.group(2)
+    author = match.group(3)
+
+    api = getTwitterAPI()
+    api.update_status('"%s" - %s' % (quote, author))
+
+  time.sleep(SECONDS_IN_DAY) 
